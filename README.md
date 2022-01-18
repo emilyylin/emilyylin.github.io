66c83318d68effcbd11d9da85a17af43953e1ed8
emilyylin <emilyylin17@gmail.com>
2022-01-18 03:51:45 -0500

    change website url

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Hi! Nice to meet you :)
+
 Just a simple site built with React! Currently a WIP
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -27,13 +27,13 @@ function LandingPage() {
           </div>
           <div className="description">
             I’m a third year software engineering student at the University of
-            Waterloo. Previously, I’ve worked as a software engineer at RBC and
-            at the TD innovation labs. I’m passionate about mobile and web dev,
+            Waterloo. Previously, I’ve worked as a software engineer at the TD
+            innovation labs and at RBC. I’m passionate about mobile and web dev,
             and I’m hoping to gain some experience in the AR/VR space!
             <br></br>
-            <br></br>I spend a lot of my free time working as a co-pres of the
-            UW Acabellas and as a backend developer for Develop for Good. My
-            other hobbies include badminton, violin, hula hooping, listening to
+            <br></br>I most of my free time working as a co-pres of the UW
+            Acabellas and as a backend developer for Develop for Good. My other
+            hobbies include badminton, violin, hula hooping, listening to
             musicals, binging tv shows, and playing the mouth trumpet.
             <br></br>
             <br></br>
